88267af59399a5f18a68ba407f3adbd4aa540cb5
UtkarshJoshiNtl <joshiutkarshntl@gmail.com>
2026-08-09 23:12:24 +0530

    test: add ExpressionEvaluator unit tests and CMake test target

---
+++ build/CMakeFiles/CalcCpp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/Code1/dev/Projects/College/CalcCpp/src/gui/main_window.cpp" "CMakeFiles/CalcCpp.dir/src/gui/main_window.cpp.o" "gcc" "CMakeFiles/CalcCpp.dir/src/gui/main_window.cpp.o.d"
+  "/home/Code1/dev/Projects/College/CalcCpp/src/main.cpp" "CMakeFiles/CalcCpp.dir/src/main.cpp.o" "gcc" "CMakeFiles/CalcCpp.dir/src/main.cpp.o.d"
+  "/home/Code1/dev/Projects/College/CalcCpp/src/operator/expression_evaluator.cpp" "CMakeFiles/CalcCpp.dir/src/operator/expression_evaluator.cpp.o" "gcc" "CMakeFiles/CalcCpp.dir/src/operator/expression_evaluator.cpp.o.d"
+  "" "CalcCpp" "gcc" "CMakeFiles/CalcCpp.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
